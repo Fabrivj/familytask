7c6dfe8b38a2b0b5044b97180ee7cf828e64b257
Fabricio Vargas <fvargasj@ucenfotec.ac.cr.com>
2026-02-03 22:55:43 -0600

    chore: initial monorepo scaffold

---
+++ backend/src/main/java/com/vertexdev/familytask/FamilytaskApplication.java
@@ -0,0 +1,13 @@
+package com.vertexdev.familytask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilytaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FamilytaskApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/vertexdev/familytask/FamilytaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vertexdev.familytask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilytaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
